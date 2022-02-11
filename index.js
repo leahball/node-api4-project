f1445c1565051be1708005e8af55dff12a287c24
iamleahb <leah.ball@gmail.com>
2022-02-10 20:36:04 -0600

    wip

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+
+const server = express();
+server.use(express.json());
+server.use(express.static(path.join(__dirname, "client/build")));
+
+server.get("/api/users", (req, res) => {
+  res.json([
+    { id: 1, username: "foo" },
+    { id: 2, username: "bar" },
+    { id: 3, username: "baz" },
+  ]);
+});
+
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
